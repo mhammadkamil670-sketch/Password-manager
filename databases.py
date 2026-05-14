4b73ec189e373877f050253ce488ce2645834aee
mhammadkamil670-sketch <mhammadkamil670@gmail.com>
2026-05-14 21:55:16 +0300

    Update databases.py

--- databases.py
+++ databases.py
@@ -3,7 +3,7 @@ import psycopg2
 import os
 load_dotenv()
 def get_con():
-    return psycopg2.connect(os.getenv("DATABASE_URL2_0"),
+    return psycopg2.connect(print(os.getenv("DATABASE_URL2_0")),
 sslmode="require")
 def something():
     from tools import decryption_master,decryption_student
