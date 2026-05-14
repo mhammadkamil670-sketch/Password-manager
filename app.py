07016d189d7acc8e5bf9a6d5eda76c1082b6efd9
mhammadkamil670-sketch <mhammadkamil670@gmail.com>
2026-05-14 20:46:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -104,6 +104,5 @@ def save_password():
         savepassword(username,password,title)   
         return jsonify({"status":True})      
 if __name__ == "__main__":
-    tablecreation()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)              
