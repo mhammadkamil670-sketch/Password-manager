1cc94da9bf5caba1e1c17013c90ecf684f7c6821
mhammadkamil670-sketch <mhammadkamil670@gmail.com>
2026-05-14 20:40:58 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 from databases import get_secretkey
 load_dotenv()
+from flask import redirect,session
 def get_cipher():
     key=os.getenv("SECRET_KEY")
     cipher=Fernet(key.encode())
@@ -23,4 +24,16 @@ def decryption_student(password,username):
     key_=decryption_master(key)
     cipher_s=Fernet(key_.encode())
     return cipher_s.decrypt(password.encode()).decode()
-            
+def check_session():
+    if not "user" in session:
+        return redirect("/")
+def check_pass_title(password,title):
+    if len(title)<6:
+        return "title should not be less than 6 characters"
+    elif len(title)>50:
+        return "title should not exceed 50 characters"
+    if len(password)>50:
+        return "password should not exceed 50 characters"
+    elif len(password)<8:
+        return "password should not be less than 8 characters"
+    return False
